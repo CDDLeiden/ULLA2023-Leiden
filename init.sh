47329b90b950b34458a3560a5bf223af498c0768
Martin Šícho <sicho.martin@gmail.com>
2023-03-10 11:41:22 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+mkdir -p .ssh
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-git clone git@github.com:CDDLeiden/ULLA2023-Leiden.git
+git clone https://github.com/CDDLeiden/ULLA2023-Leiden.git
 rm -rf ULLA2023-Leiden/assignments
