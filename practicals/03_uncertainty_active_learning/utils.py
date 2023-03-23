1b8da3a362d9bfabac1978fa61983425a1fa8bca
sohvi <s.luukkonen@lacdr.leidenuniv.nl>
2023-03-23 15:22:50 +0100

    update tutorial

--- practicals/03_uncertainty_active_learning/utils.py
+++ practicals/03_uncertainty_active_learning/utils.py
@@ -53,34 +53,3 @@ def create_dataloader(data, subset, label_col, feature_col, batch_size):
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
     return dataloader
-    
-def variance_distribution_plot(variance, title):
-    
-        fig, ax = plt.subplots(figsize=(4, 4))
-    
-        ax.hist(variance, bins=50, density=True, ec='k', histtype='stepfilled'  )
-        ax.axvline(variance.mean(), color='k', linestyle='--', linewidth=2)
-        ax.set_xlabel('Variance')
-        ax.set_ylabel('Density')
-        ax.set_title(title)
-        ax.grid(linestyle='--', alpha=0.5)
-    
-def correlation_plot(x, y, y_err, title):
-
-    fig, ax = plt.subplots(figsize=(4, 4))
-
-    xmax = max(x.max(), y.max()) +1
-    xmin = min(x.min(), y.min()) -1
-    ax.set_xlim(xmin, xmax)
-    ax.set_ylim(xmin, xmax)
-    ax.grid(linestyle='--', alpha=0.5)
-    ax.set_aspect('equal')
-
-    ax.errorbar(x, y, yerr=y_err, fmt='o', mec='k', ecolor='k', elinewidth=1, capsize=0)
-    ax.plot([xmin, xmax], [xmin, xmax], color='k', linestyle='--', linewidth=2)
-
-    ax.set_xlabel('True value')
-    ax.set_ylabel('Predicted value')
-    ax.set_title(title)
-
-    
